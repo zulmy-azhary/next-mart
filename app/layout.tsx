10699b9d966e0077cde4baf233501554c406029c
zoel-a <zulmyazhary32@gmail.com>
2024-03-06 23:46:58 +0800

    feat: add sonner for toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Open_Sans } from "next/font/google";
 import { ThemeProvider } from "@/providers/theme.provider";
 import { ModalProvider } from "@/providers/modal.provider";
 import "./tailwind.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const openSans = Open_Sans({
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <body className={openSans.className}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <Toaster />
             <ModalProvider />
             {children}
           </ThemeProvider>
