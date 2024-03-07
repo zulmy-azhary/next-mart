2c259e882067a5995725efbd65f9314ce3019919
zoel-a <zulmyazhary32@gmail.com>
2024-03-08 01:04:49 +0800

    refactor: update modal provider

--- providers/modal.provider.tsx
+++ providers/modal.provider.tsx
@@ -1,20 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 import { StoreModal } from "@/components/modals/store-modal";
 
 export const ModalProvider = () => {
-  const [isMounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return null;
-  }
-
   return (
     <>
       <StoreModal />
