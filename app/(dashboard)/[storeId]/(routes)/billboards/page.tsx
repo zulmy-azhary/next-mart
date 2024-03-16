051ba0716f3c23877012be4b09b88e06c349ea6b
zoel-a <zulmyazhary32@gmail.com>
2024-03-16 13:50:36 +0800

    refactor: format createdAt using date-fns

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import { BillboardsSection } from "./_components/billboards-section";
 import type { Params } from "@/types";
 import { db } from "@/lib/db";
+import { format } from "date-fns";
 
 export const metadata: Metadata = {
   title: "Billboards",
 };
 
 type BillboardsPageProps = Params<{ storeId: string }>;
+
 export default async function BillboardsPage({ params }: BillboardsPageProps) {
   const billboards = await db.billboard.findMany({
     where: {
@@ -17,6 +19,11 @@ export default async function BillboardsPage({ params }: BillboardsPageProps) {
       createdAt: "desc",
     },
   });
+
+  const formattedBillboards = billboards.map((item) => ({
+    ...item,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
   
-  return <BillboardsSection billboards={billboards} />;
+  return <BillboardsSection billboards={formattedBillboards} />;
 }
