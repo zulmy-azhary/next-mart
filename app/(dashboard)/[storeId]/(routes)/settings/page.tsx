ec5e08e15802273a125051552b74fd030d474957
zoel-a <zulmyazhary32@gmail.com>
2024-03-17 19:16:18 +0800

    refactor: remove repeat code

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,6 +1,5 @@
 import { db } from "@/lib/db";
 import type { Params } from "@/types";
-import { auth } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { SettingsForm } from "./_components/settings-form";
@@ -13,16 +12,10 @@ type SettingsPageProps = Params<{ storeId: string }>;
 
 export default async function SettingsPage({ params }: SettingsPageProps) {
   const { storeId } = params;
-  const { userId } = auth();
-
-  if (!userId) {
-    redirect("/sign-in");
-  }
 
   const store = await db.store.findUnique({
     where: {
       id: storeId,
-      userId,
     },
   });
 
