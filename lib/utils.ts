43c569fe404a40c336c16ddd5f9dd658250b22df
zoel-a <zulmyazhary32@gmail.com>
2024-04-18 13:53:27 +0800

    feat: create catch exception utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { NextResponse } from "next/server";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -9,3 +10,10 @@ export const formatter = new Intl.NumberFormat("en-Us", {
   style: "currency",
   currency: "USD",
 });
+
+export const catchException = (error: unknown) => {
+  return NextResponse.json(
+    { success: false, error: { message: (error as Error).message } },
+    { status: 500 }
+  );
+};
